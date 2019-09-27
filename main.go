0c9b82a5ea757bd9c10701780562da795e20593f
joshuapohan <joshuapohan93@gmail.com>
2019-09-27 13:33:13 +0700

    updated test case

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"image/png"
-	"bytes"
 	"io/ioutil"
 	"log"
 	"apng"
@@ -16,36 +14,7 @@ func logError(e error){
 	}
 }
 
-func readInputFiles(ap *apng.APNGModel){
-	files, err := ioutil.ReadDir("./input")
-
-	logError(err)
-	test := &apng.APNGModel{}
-
-	for _, fileInfo := range files{
-		f, err := os.Open("./input/" + fileInfo.Name())
-		logError(err)
-		curPng, err := png.Decode(f)
-		test.AppendImage(f)
-
-		logError(err)
-
-		curImgBuffer := new(bytes.Buffer)
-
-		if err := png.Encode(curImgBuffer, curPng); err != nil{
-			fmt.Println(err)
-			return
-		}
-
-		
-
-		
-		//curPngChunk := apng.GetPNGChunk(curImgBuffer)
-		//(*ap).chunks = append((*ap).chunks, curPngChunk)
-	}
-}
-
-func readInputFiles2(){
+func readInputFiles(){
 	files, err := ioutil.ReadDir("../input")
 
 	logError(err)
@@ -55,39 +24,18 @@ func readInputFiles2(){
 		f, err := os.Open("../input/" + fileInfo.Name())
 		logError(err)
 		test.AppendImage(f)
+		test.AppendDelay(64)
 		logError(err)
 	}
 	test.Encode()
-	test.SavePNGData()
+	test.SavePNGData("logAPNG9.png")
 }
 
 func ProcessInputFiles(){
-	//var pngs apng.APNGModel
-	//readInputFiles(&pngs)
-	//pngs.LogPNGChunks()
-	
 	fmt.Println("Writing input files")
-	readInputFiles2()
+	readInputFiles()
 }
 
 func main(){
-	/*f, _ := os.Open("./mario.png")
-	inPng, err := png.Decode(f)
-	if err != nil{
-		fmt.Println(err)
-		f.Close()
-		return
-	}
-
-	imgBuffer := new(bytes.Buffer)
-
-	if err := png.Encode(imgBuffer, inPng); err != nil{
-		fmt.Println(err)
-		return
-	}
-
-	chunk := getPNGChunk(imgBuffer)
-
-	savePNGChunk(chunk)*/
 	ProcessInputFiles()
 }
